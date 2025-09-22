939eca5eeba6ae25ca153a5b0712e3fae98965a3
bruteforce774 <danielaw@getacademy.no>
2025-09-22 12:36:37 +0200

    showcase functional code

---
+++ src/components/ColourText.ts
@@ -0,0 +1,11 @@
+export default class ColourText {
+    
+}
+
+// export function ColourText(text: string, colour: string) : HTMLElement {
+//     const element = document.createElement('div');
+//     element.classList.add('colour-text');
+//     element.textContent = text;
+//     element.style.color = colour;
+//     return element;
+// }
